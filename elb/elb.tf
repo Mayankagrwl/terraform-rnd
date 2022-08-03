d57c531325dc3bc77e68f790fc7781fa14d5ffa5
Mayank <mayankagrwl@gmail.com>
2022-08-03 12:06:38 +0530

    second commit

---
+++ elb/elb.tf
@@ -0,0 +1,63 @@
+module "lb-http" {
+  source  = "../module/"
+  name    = var.lb_name
+  project = var.project_id
+
+
+  backends = {
+    default = {
+      description = null
+      groups = [
+        {
+          group = google_compute_region_network_endpoint_group.serverless_neg.id
+        }
+      ]
+      enable_cdn              = false
+      security_policy         = null
+      custom_request_headers  = null
+      custom_response_headers = null
+
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = ""
+        oauth2_client_secret = ""
+      }
+      log_config = {
+        enable      = false
+        sample_rate = null
+      }
+    }
+  }
+}
+
+resource "google_compute_region_network_endpoint_group" "serverless_neg" {
+  provider              = google-beta
+  name                  = var.neg_name 
+  network_endpoint_type = "SERVERLESS"
+  region                = var.region
+  cloud_run {
+    service = google_cloud_run_service.default.name
+  }
+}
+
+resource "google_cloud_run_service" "default" {
+  name     = var.cloud_run_svc 
+  location = var.region
+  project  = var.project_id
+
+ template {
+    spec {
+      containers {
+        image = "gcr.io/cloudrun/hello"
+      }
+    }
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "public-access" {
+  location = google_cloud_run_service.default.location
+  project  = google_cloud_run_service.default.project
+  service  = google_cloud_run_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
---
+++ elb/variables.tf
@@ -0,0 +1,33 @@
+variable "project_id" {
+  description = "Project_ID"
+  type        = string
+  default = "qwiklabs-gcp-01-fae936e43149"
+}
+
+variable "lb_name" {
+  description = "Name of lB"
+  type        = string
+  default =  "myehs-dev-external-glb-temp"
+}
+
+variable "region" {
+  type        = string
+  description = "Define region"
+  default     = "us-central1"
+}
+
+variable "neg_name" {
+  type  = string
+  description = "NEG name"
+  default = "myehs-dev-apigw-neg-temp"
+
+variable "backend_name" {
+  type  = string
+  description = " Backend name for External load Balance"
+  default = "myehs-dev-apigw-bes"
+}
+
+variable "cloud_run_svc" {
+  type = string
+  description = "Cloud run service name"
+  default = "myehs-dev-appui-bes" 
